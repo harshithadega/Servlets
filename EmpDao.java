8079a549ac0500d08b93424c6567df4cef7ed071
harshithadega <37622329+harshithadega@users.noreply.github.com>
2020-01-17 00:07:46 -0800

    Add files via upload

---
+++ EmpDao.java
@@ -0,0 +1,18 @@
+package com.deloitte.emp.dao;
+import java.sql.Date;
+import java.util.List;
+
+	import com.deloitte.emp.beans.Emp;
+
+	public interface EmpDao {
+		void openConnection();
+		void close();
+		int addEmp(Emp emp); 
+		List<Emp> getEmpls();
+		Emp getEmpByCode(int empcode);
+	    void delEmp(int empcode);
+	    void updEmp(double sal,int empcode);
+	    int updAll(String ename,String job,int mgr,Date hiredate,double sal,double comm,int deptno,int empno);
+	    int updEmp(Emp emp);
+	}
+
---
+++ EmpUtils.java
@@ -0,0 +1,12 @@
+package com.deloitte.emp.utils;
+
+public class EmpUtils {
+	public static String insertqry="insert into emp values(?,?,?,?,?,?,?,?)";
+	public static final String GETEMPLS = "select * from emp";
+	public static final String GETEMPlBYCODE = "select * from emp where empno=?";
+	public static final String DELROW = "delete from emp where empno =?";
+	public static final String UPDSAL="update emp set sal=? where empno =?";
+	public static final String UPDALL="update emp set ename=?,job=?,mgr=?,hiredate=?,sal=?,comm=?,deptno=? where empno=?";
+	public static final String UPDEMP="update emp set ename=?,job=? where empno =?";
+
+}
